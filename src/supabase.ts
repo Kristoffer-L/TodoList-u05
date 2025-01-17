bb9692f755f0f503d1ec0200e3a63cda55272373
Kristoffer-L <kristoffer.larsson.02@gmail.com>
2025-01-17 17:09:42 +0100

    commit

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL: string = 'https://oztjzscfexuysiadcuzp.supabase.co'; // Ersätt med din Supabase-URL
-const SUPABASE_ANON_KEY: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dGp6c2NmZXh1eXNpYWRjdXpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwMjY0ODYsImV4cCI6MjA1MjYwMjQ4Nn0.xsfo1W0hJSzYREYoBD3PYN79SuLd_8whoEwTECbQLH8'; // Ersätt med din Anon-nyckel
+const SUPABASE_URL: string = "https://oztjzscfexuysiadcuzp.supabase.co"; // Ersätt med din Supabase-URL
+const SUPABASE_ANON_KEY: string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96dGp6c2NmZXh1eXNpYWRjdXpwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcwMjY0ODYsImV4cCI6MjA1MjYwMjQ4Nn0.xsfo1W0hJSzYREYoBD3PYN79SuLd_8whoEwTECbQLH8"; // Ersätt med din Anon-nyckel
 
 // Skapa Supabase-klienten
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
